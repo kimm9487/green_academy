28131cd5a3c8c1a5bd69fdae46925d5c1ac1c865
manic-066g <manic-066g@manic-066>
2023-06-26 15:02:05 +0900

    맵

---
+++ gyuhojava/src/fullstack/util/collectionexam/MapExam.java
@@ -0,0 +1,23 @@
+package fullstack.util.collectionexam;
+
+public class MapExam {
+
+	public static void main(String[] args) {
+		/*
+		 * Map Interface : Map 의 collection 의 직접 하이 타입은 아니지만,
+		 * 내부에 여러 메서드를 통해 필요시 key, value, key and value 전체를
+		 * collection 타입으로 리턴하도록 정의하고 있습니다.(호환성)
+		 * 
+		 * Map의 대표적 클래스로는 HashMap 이 있고, 초기부터 사용하던 HashTable 은
+		 * HashMap 으로 대체되었다고 보면 됩니다.
+		 * 
+		 * Map 은 데이터(객체)를 key로 관리하고, key는 중복 불가입니다.(덮어씀)
+		 * 이러한 이유로 Map 은 순차적(index) 등을 통한 Element 접근은 불가능하고
+		 * 오직 key 로만 value 를 얻어낼 수 있습니다.
+		 * 
+		 * 만약 타입 변환이 필요하다면, 메서드를  통해 Collection 타입의 요소로 변환 가능합니다.
+		 */
+
+	}
+
+}
